20360d16ca6c31d94e2b6c9033feaf9eb26d47b9
Earnpoint <63481667+sadi200@users.noreply.github.com>
2025-04-17 15:04:07 -0600

    Update gensynEP.sh

--- gensynEP.sh
+++ gensynEP.sh
@@ -30,9 +30,8 @@ sudo apt update && sudo apt install -y yarn
 # Run external script
 curl -sSL https://raw.githubusercontent.com/sadi200/yy/refs/heads/main/node.sh | bash
 
+# Start a new screen session
+screen -S gensyn
+
 # Clone and run gensyn testnet script
-cd $HOME
-rm -rf gensyn-testnet
-git clone https://github.com/zunxbt/gensyn-testnet.git
-chmod +x gensyn-testnet/gensyn.sh
-./gensyn-testnet/gensyn.sh
+cd $HOME && rm -rf gensyn-testnet && git clone https://github.com/zunxbt/gensyn-testnet.git && chmod +x gensyn-testnet/gensyn.sh && ./gensyn-testnet/gensyn.sh
